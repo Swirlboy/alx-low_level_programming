b3d7bc63442107d590bdf4a010a1dc872e11c237
Swirlboy <martynmuyah@gmail.com>
2022-03-17 05:26:09 -0700

    C - More functions, more nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - returns the product of its parameters
+ * @i: int type number
+ * @j: int type number
+ * Return:0
+ */
+
+
+int mul(int i, int j)
+{
+return (i * j);
+}
